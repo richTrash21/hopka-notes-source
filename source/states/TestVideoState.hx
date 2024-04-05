17e0c5c1f0bb4e750e566ada4d9d05b3994eea23
Redar13 <93761434+Redar13@users.noreply.github.com>
2024-04-05 20:00:00 +0500

    `@richTrash21`

--- source/states/TestVideoState.hx
+++ source/states/TestVideoState.hx
@@ -20,7 +20,8 @@ class TestVideoState extends flixel.FlxState
 		"https://cdn.discordapp.com/attachments/1063500919469252709/1224057863501254837/This_is_probably_the_worst_dub_Ive_ever_made.mp4.mp4?ex=661c1bb8&is=6609a6b8&hm=c0155b4aa4ef6412212b4983ee3e3a87c84a49dac149806f08ec439b34c2cd77&",
 		"https://cdn.discordapp.com/attachments/1063500919469252709/1224066052564975766/HopKa_-_5rubles_choki_choki_animation_meme_in_fnf_bezdari_fridaynightfunkin_fnfmod_fnf_Mat.mp4?ex=661c2358&is=6609ae58&hm=268e6897d460640ca6e4e9cf7784337ba31a480ac00b0eb826f5426c9d9d4e4f&",
 		"https://cdn.discordapp.com/attachments/791373867897192459/1214602136122429521/video0.mov?ex=661564e3&is=6602efe3&hm=d08c39cd1482d5ca6999b933e190ec9cc341925dbff8a42bdf30e217f4f0835e&",
-		"https://cdn.discordapp.com/attachments/791373867897192459/1225073961545498695/WAS_THAT_THE_BAIAIAIAISYUHAIAIAIIAHU.mp4?ex=661fce09&is=660d5909&hm=b01db1807981063c75cb60cd9753d25c26c777fe3435ff515f5e55c2904374b0&"
+		"https://cdn.discordapp.com/attachments/791373867897192459/1225073961545498695/WAS_THAT_THE_BAIAIAIAISYUHAIAIAIIAHU.mp4?ex=661fce09&is=660d5909&hm=b01db1807981063c75cb60cd9753d25c26c777fe3435ff515f5e55c2904374b0&",
+		"https://cdn.discordapp.com/attachments/791373867897192459/1225821395250843739/gummy_elephant.mp4?ex=66228623&is=66101123&hm=40ec5a0ec0d439cc5e6584f8376c1ed5dd92eee5aaa08c02806a43dab3bc5852&"
 	];
 	var video:VideoSprite;
 
